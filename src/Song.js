3d04c430892f1230b2b26d72279a72ccffd9363f
Zeynep <zeynep.aykal@ssh.com>
2020-10-04 16:03:56 +0300

    css changes

--- src/Song.js
+++ src/Song.js
@@ -5,27 +5,18 @@ import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 const Song = ({ song, isFavorite, onFavoriteChange }) => {
   return (
     <div key={song.id} className="song-item">
-    <div className="col-5">
+      <div className="col-5">
+        <img className="thumbnail" src={song.images} alt="..."></img>
 
-      
-       
-        
-         <div className="thumbnail">
-           <img src={song.images} alt="..."></img>
-         </div> 
-        
-          {song.artist} - {song.title}
+              {song.artist} - {song.title}
         
           <span className="badge badge-primary badge-pill">{song.level}</span>
-        
 
         <button onClick={onFavoriteChange}>
           {isFavorite ? <MdFavorite id="heart-full"/> : <MdFavoriteBorder id="heart-empty"/> } 
         </button>
-     
-    
+      </div>
     </div>
-  </div>
   );
 };
 
